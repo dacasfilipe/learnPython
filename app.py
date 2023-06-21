4a1e1de6904bfe08ae2e938a17bb0aab0b921491
dacasfilipe <filiperdacas@gmail.com>
2023-06-20 23:34:37 -0300

    declaração de variáveis

---
+++ app.py
@@ -0,0 +1,28 @@
+velocidade_internet = 10
+print(velocidade_internet)
+#Quais tipos de dados são possíveis armazenar?
+#Pyhton é case sensitive
+#números
+#booleanos
+esta_aberta = True
+#Strings
+slogan = 'Feito é melhor que perfeito'
+slogan2 = "Feito é melhor que perfeito"
+a,b,c,d = 1,2,3,4 #processo de unpacking
+print(a)
+print(type(a))
+
+#Desafio
+variavel_1 = 25.87
+variavel_2 = True
+variavel_3 = 'Bom dia!'
+variavel_4 = 15
+
+print('Tipo das variáveis')
+print(type(variavel_1))
+print(type(variavel_2))
+print(type(variavel_3))
+print(type(variavel_4))
+
+
+
