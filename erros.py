4e7d7bc09db294397907ea22e3c928ed80fe01a6
dacasfilipe <filiperdacas@gmail.com>
2023-06-20 23:57:17 -0300

    adiconado erros.py

---
+++ erros.py
@@ -0,0 +1,9 @@
+#Erro genérico
+# 1 - leia o erro
+# 2 - copie a msg e cole no google tradutor
+# 3 - Tente entender o que o erro significa
+# 4 - Copie o erro e coloque Python antes da mensagem para pesquisar no google
+# 5 - pesquisar em inglês sempre será melhor
+# 
+
+#Erro específico (inclui dados da sua aplicação)
