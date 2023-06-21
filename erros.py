c40c9566a23634aed972c0bfe41ad573f9e9aed2
dacasfilipe <filiperdacas@gmail.com>
2023-06-20 23:59:58 -0300

    ok

--- erros.py
+++ erros.py
@@ -4,6 +4,9 @@
 # 3 - Tente entender o que o erro significa
 # 4 - Copie o erro e coloque Python antes da mensagem para pesquisar no google
 # 5 - pesquisar em inglês sempre será melhor
-# 
+# 6 - copie o erro e cole no chatgpt
+# pesquise primeiro no google depois no chatgpt
+
+
 
 #Erro específico (inclui dados da sua aplicação)
