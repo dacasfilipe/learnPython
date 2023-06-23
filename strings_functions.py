5ff51cae687c78341db9a3674749399a592f4a74
dacasfilipe <filiperdacas@gmail.com>
2023-06-22 23:53:53 -0300

    functions string

---
+++ strings_functions.py
@@ -0,0 +1,8 @@
+nome_curso = 'Python'
+print(nome_curso.upper())
+print(nome_curso.lower())
+print(nome_curso.strip())
+print(nome_curso.lstrip())
+print(nome_curso.find('Py'))
+print(nome_curso.replace('Python', 'Linguagem'))
+print('https://sc.olx.com.br/?o=90&q=relogio'.replace('relogio', 'carro'))
