268be1f1aae037e2d772cd38f95b4bfb2b0952e7
dacasfilipe <filiperdacas@gmail.com>
2023-06-22 23:37:42 -0300

    strings

---
+++ strings.py
@@ -0,0 +1,19 @@
+print('codar todos os dias')
+print("codar todos os dias")
+print("""codar todos os dias
+      e estou aprendendo muito!""")
+#Caracteres de escape
+print('Olá meu nome é \nFilipe')
+#ignorar caracteres
+print('Codar todos \'os dias')
+#fazer com que caracter apareça
+print('Arquivo localizado em \\c:drive\\arquivo.txt')
+nome='Filipe'
+print(len(nome))
+#comentar um trecho de código
+"""
+    comentario
+    de varias linhas
+"""
+
+
