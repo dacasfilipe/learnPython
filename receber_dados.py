a2ae1d35e50adcc7908fa275d851da92067591d3
dacasfilipe <filiperdacas@gmail.com>
2023-06-24 23:23:18 -0300

    criado arquivo recebe_dados

---
+++ receber_dados.py
@@ -0,0 +1,9 @@
+#recebendo dados do teclado
+nome = input('Digite seu nome: ')
+#obs: todos dados vem como string
+#caso precise faça a conversão ex: 
+#nome = int(input('Digite seu nome: '))
+print(nome)
+print(type(nome))
+
+
