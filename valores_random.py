db9ac72c065a3cb2f9e8d7925b8a05664594e3b8
dacasfilipe <filiperdacas@gmail.com>
2023-06-25 22:14:36 -0300

    finalizado valores aleatórios

--- valores_random.py
+++ valores_random.py
@@ -5,3 +5,16 @@ print(random.random())
 
 # Gera um valor decimal de Valor mínimo a Valor máximo
 print(random.uniform(4,10))
+
+#Gera um valor inteiro
+print(random.randint(4,10))
+
+#escolher cor aleatória
+cores = ['verde','vermelho','azul']
+print(random.choices(cores, k=2))
+
+#embaralhar as cartas
+cartas_baralho = ['carta1','carta2','carta3','carta4','carta5']
+print(random.shuffle(cartas_baralho))
+print(cartas_baralho)
+
