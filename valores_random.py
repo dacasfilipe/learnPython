da27d776935bac2ea8bdbe099010464a475a564e
dacasfilipe <filiperdacas@gmail.com>
2023-06-25 22:05:42 -0300

    valores random em python

---
+++ valores_random.py
@@ -0,0 +1,7 @@
+# valores aleatórios com random
+import random
+# Gera um valor de 0.0 a 1.0
+print(random.random())
+
+# Gera um valor decimal de Valor mínimo a Valor máximo
+print(random.uniform(4,10))
