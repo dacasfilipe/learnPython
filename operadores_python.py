9cbb281bbdff2e5f85b22ef930f598dc7b7ee366
dacasfilipe <filiperdacas@gmail.com>
2023-06-30 07:54:48 -0300

    adicionado operadores booleanos

--- operadores_python.py
+++ operadores_python.py
@@ -11,3 +11,14 @@ print(True == False)
 print('Rafael' == 'rafael')
 print('b'>'a')
 
+#operadores booleanos
+maior_de_idade = True
+possui_carteira_de_trabalho = True
+esta_trabalhando_atualmente = True
+possui_veiculo_proprio = False
+#você só pode trabalhar se for maior de idade e possuir carteira de trabalho
+print(maior_de_idade == True and possui_carteira_de_trabalho == True)
+print(maior_de_idade and possui_carteira_de_trabalho)
+#Queremos contratar pessoas que ainda não possuem veículo próprio, mas
+#já possuam uma carteira de trabalho
+print(possui_carteira_de_trabalho == True and not possui_veiculo_proprio)
