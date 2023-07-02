20fad19c455d5b8c58a11190ac9fbb7abf9a21fd
dacasfilipe <filiperdacas@gmail.com>
2023-07-02 09:41:20 -0300

    adicionado arquivo convesao de tipos

---
+++ conversao_tipos.py
@@ -0,0 +1,8 @@
+#conversao de tipos
+idade = input('Qual é a sua idade?')
+print(int(idade) > 18)
+
+print(type(str(5)))
+#receber valores decimais
+altura_da_parede = input('Qual é a altura da parede?')
+print(float(altura_da_parede)>2.10)
